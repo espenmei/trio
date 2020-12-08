f47f8eea063f930a315d071a0ebcb7bbf1e96fb9
Espen Moen Eilertsen <espenmei@uio.no>
2020-12-08 13:10:31 +0100

    first

---
+++ trio.R
@@ -0,0 +1,60 @@
+library(genio) # Read plink format
+library(OpenMx) # Fit model
+library(gaston) # Compute GRM
+
+setwd("C:/Users/espenmei/OneDrive - Universitetet i Oslo/Prosjekter/GCTA/MFGCTA/BehaviorGenetics/V3/github")
+
+# Read data
+dat = read.bed.matrix("simdat")
+
+# Compute GRM
+A = GRM(dat)
+
+# Subset GRM blocks
+mid = 1:1000
+pid = 1001:2000
+oid = 2001:3000
+Amm = A[mid, mid]
+App = A[pid, pid]
+Aoo = A[oid, oid]
+Dpm = A[pid, mid] + A[mid, pid]
+Dom = A[oid, mid] + A[mid, oid]
+Dop = A[oid, pid] + A[pid, oid]
+
+# Set up model with OpenMx
+yX = cbind(dat@ped$pheno[oid], 1)
+colnames(yX) = c("y", "x")
+K = 1000 # Number of trios
+mod = mxModel("trio",
+              mxMatrix("Lo", 3, 3, T, sqrt(diag(var(yX[, 1]) / 4, 3)), c("l11", "l21", "l31", "l22", "l32", "l33"), name = "L"),
+              mxAlgebra(L %*% t(L), name = "Sg"),
+              mxMatrix("Fu", 1, 1, T, var(yX[, 1]) / 4, "se", name = "Se"),
+              
+              mxMatrix("Sy", K, K, F, Amm, name = "Amm"),
+              mxMatrix("Sy", K, K, F, App, name = "App"),
+              mxMatrix("Sy", K, K, F, Aoo, name = "Aoo"),
+              mxMatrix("Sy", K, K, F, Dpm, name = "Dpm"),
+              mxMatrix("Sy", K, K, F, Dom, name = "Dom"),
+              mxMatrix("Sy", K, K, F, Dop, name = "Dop"),
+              mxMatrix("Id", K, K, name = "I"),
+              mxData(yX, "raw", sort = F),
+              
+              mxAlgebra(Sg[1, 1] * Amm + Sg[2, 2] * App + Sg[3, 3] * Aoo +
+                          Sg[2, 1] * Dpm + Sg[3, 1] * Dom + Sg[3, 2] * Dop +
+                          se * I, name = "V"),
+              mxExpectationGREML("V", dataset.is.yX = T),
+              mxFitFunctionGREML())
+
+# Fit the model
+fitopenmx = mxRun(mod)
+summary(fitopenmx)
+mxEval(Sg, fitopenmx)
+
+# The same model can be fit with the gaston package
+# However, it not possible to force the genetic effects to have a valid covariance matrix as in the OpenMx model
+Rs = list(Amm, App, Aoo, Dpm, Dom, Dop)
+fitgaston = lmm.aireml(yX[, 1], K = Rs)
+fitgaston$BLUP_beta
+fitgaston$tau
+fitgaston$sigma2
+fitgaston$logL
