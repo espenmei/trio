2e3dbd8953600ec7153e63fccc6fe12ca15c8ca2
Espen Moen Eilertsen <e.m.eilertsen@psykologi.uio.no>
2020-12-15 20:49:41 +0100

    files

--- trio.R
+++ trio.R
@@ -2,8 +2,10 @@ library(genio) # Read plink format
 library(OpenMx) # Fit model
 library(gaston) # Compute GRM
 
+setwd("C:/Users/espenmei/OneDrive - Universitetet i Oslo/Prosjekter/GCTA/MFGCTA/BehaviorGenetics/V3/github")
+
 # Read data
-dat = read.bed.matrix("simdat")
+dat = read.bed.matrix("simtrio")
 
 # Compute GRM
 A = GRM(dat)
